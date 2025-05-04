881764015972c05df32b15a809e9897fdbdf4851
Ayman Shakil <ayman.x.shakil@gsk.com>
2025-05-04 15:32:54 +0530

    update color for spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import { FadeLoader } from "react-spinners";
 const Loading = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <FadeLoader color="#f36633" />
+      <FadeLoader color="#2563eb" />
       <p className="mt-4 text-lg text-gray-700">Loading...</p>
     </div>
   );
